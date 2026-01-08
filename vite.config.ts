0da3edcc58e126b26a4b443d1a04cd8917d147a5
茅睿 <maorui1@60305333m.domain.sensetime.com>
2026-01-08 18:45:48 +0800

    chore: update vite config for tauri compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,21 @@ import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Prevent vite from obscuring rust errors
+  clearScreen: false,
+  // Tauri expects a fixed port, fail if that port is not available
+  server: {
+    strictPort: true,
+  },
+  // to access the Tauri environment variables set by the CLI with information about the current target
+  envPrefix: ['VITE_', 'TAURI_'],
   build: {
-    sourcemap: 'hidden',
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
   plugins: [
     react({
